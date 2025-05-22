8a7fe0e7f0147f6c452338658f8f0527ec96b095
Marta Aleksa Valaine <a230440mv@rvt.lv>
2025-05-23 00:46:30 +0300

    Pbeikta sekcija "Ceļvedis"

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,18 @@
 document.getElementById("poga_par").addEventListener("click", function () {
     window.location.href="#informacija";
 });
+
+const izvele = document.getElementById("izvele");
+const saturi = document.querySelectorAll(".saturi")
+
+izvele.addEventListener('change', () => {
+    const vertiba = izvele.value;
+    saturi.forEach(s => s.style.display = 'none');
+    if (vertiba) {
+        const aktivais = document.getElementById(vertiba);
+        if (aktivais) {
+            aktivais.style.display = 'flex';
+            aktivais.style.flexWrap = "wrap";
+        }
+    }
+});
