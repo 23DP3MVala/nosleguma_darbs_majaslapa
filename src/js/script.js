98abb714d39fffa8756ca3aa1158a00ff43875dc
Marta Aleksa Valaine <a230440mv@rvt.lv>
2025-05-23 02:14:22 +0300

    Pievienota responsivitāte

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,22 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav');
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+navLinks.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+      navLinks.classList.remove('active');
+    });
+});
+
+document.addEventListener('click', (event) => {
+    const isClickInside = navLinks.contains(event.target) || hamburger.contains(event.target);
+    if (!isClickInside) {
+      navLinks.classList.remove('active');
+    }
+});
+
 document.getElementById("poga_par").addEventListener("click", function () {
     window.location.href="#informacija";
 });
